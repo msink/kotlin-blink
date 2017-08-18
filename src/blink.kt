cff7400618b6f440df66a655624c11b2b05c6399
Mike Sinkovsky <msink@permonline.ru>
2017-08-18 10:41:54 +0500

    blink.kt: remove semicolons

--- src/blink.kt
+++ src/blink.kt
@@ -3,12 +3,12 @@ import lib.*
 const val BLINK_LED = 0
 
 fun main(args: Array<String>) {
-        gpio_request(BLINK_LED, "blink-led");
-        gpio_direction_output(BLINK_LED, 1);
+        gpio_request(BLINK_LED, "blink-led")
+        gpio_direction_output(BLINK_LED, 1)
         while (true) {
-                mdelay(200);
-                gpio_set_value(BLINK_LED, 0);
-                mdelay(200);
-                gpio_set_value(BLINK_LED, 1);
+                mdelay(200)
+                gpio_set_value(BLINK_LED, 0)
+                mdelay(200)
+                gpio_set_value(BLINK_LED, 1)
         }
 }
